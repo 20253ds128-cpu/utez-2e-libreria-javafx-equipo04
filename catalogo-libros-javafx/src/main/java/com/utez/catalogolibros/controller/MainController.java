dfdb03925b949a6f48f07efe452e2b0ead726c61
Luis Fernando Camacho <20253ds128@utez.edu.mx>
2026-03-22 16:30:22 -0600

    feat: Configuración de TableView y carga de datos de prueba

--- catalogo-libros-javafx/src/main/java/com/utez/catalogolibros/controller/MainController.java
+++ catalogo-libros-javafx/src/main/java/com/utez/catalogolibros/controller/MainController.java
@@ -1,8 +1,12 @@
 package com.utez.catalogolibros.controller;
 
 import com.utez.catalogolibros.model.Libro;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MainController {
 
@@ -44,7 +48,22 @@ public class MainController {
 
     @FXML
     public void initialize() {
-        System.out.println("Controller cargado correctamente");
+
+        colIsbn.setCellValueFactory(new PropertyValueFactory<>("isbn"));
+        colTitulo.setCellValueFactory(new PropertyValueFactory<>("titulo"));
+        colAutor.setCellValueFactory(new PropertyValueFactory<>("autor"));
+        colAnio.setCellValueFactory(new PropertyValueFactory<>("anio"));
+        colGenero.setCellValueFactory(new PropertyValueFactory<>("genero"));
+        colDisponible.setCellValueFactory(new PropertyValueFactory<>("disponible"));
+
+        ObservableList<Libro> lista = FXCollections.observableArrayList(
+                new Libro("123", "El Principito", "Antoine de Saint-Exupéry", 1943, "Ficción", true),
+                new Libro("456", "1984", "George Orwell", 1949, "Distopía", true),
+                new Libro("789", "Clean Code", "Robert C. Martin", 2008, "Programación", false)
+        );
+
+        tablaLibros.setItems(lista);
+
     }
 
 }
