4e7766b3fd511bbfb375fcc3b9a4b5db6b9ba09d
Emiliano_2E <20253ds125@utez.edu.mx>
2026-04-09 07:39:06 -0600

    MainApp terminado

--- catalogo-libros-javafx/src/main/java/com/biblioteca/MainApp.java
+++ catalogo-libros-javafx/src/main/java/com/biblioteca/MainApp.java
@@ -1,4 +1,41 @@
 package com.biblioteca;
 
-public class MainApp {
+import com.biblioteca.services.LibroServices;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainApp extends Application {
+
+    private static LibroServices libroServices;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException{
+
+        libroServices = new LibroServices();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("libro.fxml"));
+
+        Scene scene = new Scene(loader.load(), 900,600);
+
+        scene.getStylesheets().add(getClass().getResource("libro.css").toExternalForm());
+
+        primaryStage.setTitle("Biblioteca Escolar - Catalogo de libros");
+        primaryStage.setScene(scene);
+        primaryStage.setMinWidth(800);
+        primaryStage.setMaxHeight(500);
+        primaryStage.show();
+    }
+
+    public static LibroServices getLibroServices(){
+        return libroServices;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
